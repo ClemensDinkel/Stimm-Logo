e0bbf56cb2736004a7746d9f265df3950b62755e
ClemensDinkel <cle.dinkel@gmail.com>
2023-01-28 17:12:44 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./Header";
+
+export const App = () => {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <Header />
+    </div>
+  );
+};
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from "./assets/Corinna Dinkel-01.svg";
+import { Bars3Icon as BurgerMenu } from "@heroicons/react/24/outline";
+
+export const Header = () => {
+  return (
+    <div className="flex items-end">
+      <Logo className="h-[200px]" />
+      <BurgerMenu className="h-[60px] pb-4" />
+    </div>
+  );
+};
