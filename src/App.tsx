d6c1f488a2db9cbec5805ad9e3244d1fecc57344
ClemensDinkel <cle.dinkel@gmail.com>
2023-02-08 20:20:50 +0100

    nac with mock content

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { ReactComponent as Logo } from './assets/Corinna Dinkel-01.svg'
+import { ContentArea } from './ContentArea'
 import { Menu } from './Menu'
 
 export const App = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center overflow-auto">
+    <div id="top" className="flex h-full w-full flex-col items-center justify-center overflow-auto">
       <Menu />
       <Logo className="h-[200px]" />
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro id laudantium, ex facere natus adipisci neque pariatur totam, maxime maiores error enim possimus perspiciatis cupiditate odit? Perspiciatis impedit dolore laudantium.</p>
+      <ContentArea />
     </div>
   )
 }
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,21 +1,32 @@
 import { Popover } from '@headlessui/react'
 import { Bars3Icon as BurgerMenu } from '@heroicons/react/24/outline'
-
+import { clsx } from 'clsx'
 export const Menu = () => {
   return (
     <Popover className="fixed top-0 flex w-full flex-col">
-      <Popover.Button className="w-fit self-end rounded-lg bg-white/80">
-        <BurgerMenu className='h-[60px]' />
-      </Popover.Button>
-      <Popover.Panel className="z-10 bg-white">
-        <div className='flex flex-col'>
-          <a href="http://127.0.0.1:5173/a">Test A</a>
-          <a href="http://127.0.0.1:5173/b">Test B</a>
-          <a href="http://127.0.0.1:5173/c">Test C</a>
-          <a href="http://127.0.0.1:5173/d">Test D</a>
-          <a href="http://127.0.0.1:5173/e">Test D</a>
+      {({ open, close }) => (
+        <div className={clsx('flex w-full flex-col', { 'bg-white/90': open })}>
+          <Popover.Button className="z-20 mr-4 w-fit self-end rounded-lg bg-white/80">
+            <BurgerMenu className='h-[60px]' />
+          </Popover.Button>
+          <Popover.Panel className="z-10 rounded-lg p-4">
+            <div className='flex flex-wrap gap-8'>
+              <div className='flex gap-8'>
+                <a href="#top" onClick={close}>Link A</a>
+                <a href="#a" onClick={close}>Link B</a>
+              </div>
+              <div className='flex gap-8'>
+                <a href="#b" onClick={close}>Link C</a>
+                <a href="#b" onClick={close}>Link D</a>
+              </div>
+              <div className='flex gap-8'>
+                <a href="#c" onClick={close}>Link E</a>
+                <a href="#c" onClick={close}>Link F</a>
+              </div>
+            </div>
+          </Popover.Panel>
         </div>
-      </Popover.Panel>
+      )}
     </Popover>
   )
 }
