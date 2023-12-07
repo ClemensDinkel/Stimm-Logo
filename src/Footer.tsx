3c9ea0000d7a22a27b07ca18cfb62a6a8566f8f7
@Clemens.Dinkel <cle.dinkel@gmail.com>
2023-12-06 21:37:20 -0500

    add telephone and email functionality

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,8 +3,8 @@ import { Icon } from './components/Image'
 export const Footer = () => {
   return (
     <div className="fixed bottom-0 left-0 flex w-full items-center justify-center gap-4 rounded-t-lg bg-[#56070C]/95 p-4 text-xs text-white">
-      <Icon src='/Icon_Telefon.png' alt="Telefon" /><span>+49 (0) 7433/383443</span>
-      <Icon src='/Icon_E-Mail.png' alt="Email" /><span>info@stimmcoaching-logopaedie.de</span>
+      <Icon src='/Icon_Telefon.png' alt="Telefon" /><a href="tel:+497433383443">+49 (0) 7433/383443</a>
+      <Icon src='/Icon_E-Mail.png' alt="Email" /><a href="mailto:info@stimmcoaching-logopaedie.de">info@stimmcoaching-logopaedie.de</a>
     </div>
   )
 }
