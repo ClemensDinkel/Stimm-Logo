f027834d24553217cb4e756311d3a7e14ac61295
@Clemens.Dinkel <cle.dinkel@gmail.com>
2023-12-06 21:59:43 -0500

    improve footer layout

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,9 +2,15 @@ import { Icon } from './components/Image'
 
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 flex w-full items-center justify-center gap-4 rounded-t-lg bg-[#56070C]/95 p-4 text-xs text-white">
-      <Icon src='/Icon_Telefon.png' alt="Telefon" /><a href="tel:+497433383443">+49 (0) 7433/383443</a>
-      <Icon src='/Icon_E-Mail.png' alt="Email" /><a href="mailto:info@stimmcoaching-logopaedie.de">info@stimmcoaching-logopaedie.de</a>
+    <div className="fixed bottom-0 left-0 flex w-full items-center justify-center gap-2 rounded-t-lg bg-[#56070C]/95 py-4 px-2 text-xs text-white">
+      <a href="tel:+497433383443" className='flex w-fit items-center gap-2'>
+        <Icon src='/Icon_Telefon.png' alt="Telefon" />
+        <div>+49 (0) 7433/383443</div>
+      </a>
+      <a href="mailto:info@stimmcoaching-logopaedie.de" className='flex w-fit items-center gap-2'>
+        <Icon src='/Icon_E-Mail.png' alt="Email" />
+        info@&#8203;stimmcoaching-&#8203;logopaedie.de
+      </a>
     </div>
   )
 }
